930f5591ddfe928c01a7077f9282e49c19e79355
Jacques Bouthillier <lmcbout@gmail.com>
2015-09-10 09:29:42 -0400

    -Add a text box to show the history message
    Change-Id: Ief40efd6275b4c710c7f29e854196e75c7af6fb3
    Signed-off-by: Jacques Bouthillier <lmcbout@gmail.com>

--- org.eclipse.egerrit.ui/src/org/eclipse/egerrit/ui/internal/tabs/HistoryTabView.java
+++ org.eclipse.egerrit.ui/src/org/eclipse/egerrit/ui/internal/tabs/HistoryTabView.java
@@ -23,13 +23,21 @@ import org.eclipse.egerrit.ui.internal.table.UIHistoryTable;
 import org.eclipse.egerrit.ui.internal.table.provider.HistoryTableLabelProvider;
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.jface.databinding.viewers.ViewerSupport;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Text;
 
 /**
  * This class is used in the editor to handle the Gerrit history view
@@ -39,6 +47,10 @@ import org.eclipse.swt.widgets.TabItem;
 public class HistoryTabView {
 
 	private TableViewer tableHistoryViewer;
+
+	private Text msgTextData;
+
+	private ScrolledComposite sc_msgtxt = null;
 
 	// ------------------------------------------------------------------------
 	// Constructor and life cycle
@@ -60,20 +72,63 @@ public class HistoryTabView {
 	}
 
 	private void historyTab(TabFolder tabFolder, List<ChangeMessageInfo> listMessages) {
+
 		TabItem tbtmHistory = new TabItem(tabFolder, SWT.NONE);
 		tbtmHistory.setText("History");
 
-		Group group = new Group(tabFolder, SWT.NONE);
+		final Group group = new Group(tabFolder, SWT.NONE);
 		tbtmHistory.setControl(group);
 
 		GridData grid = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
 		group.setLayoutData(grid);
 		group.setLayout(new GridLayout(1, false));
 
+		SashForm sashForm = new SashForm(group, SWT.VERTICAL);
+		sashForm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
 		UIHistoryTable tableUIHistory = new UIHistoryTable();
-		tableUIHistory.createTableViewerSection(group, grid);
+		tableUIHistory.createTableViewerSection(sashForm, grid);
 
 		tableHistoryViewer = tableUIHistory.getViewer();
+		tableHistoryViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				IStructuredSelection sel = (IStructuredSelection) event.getSelection();
+				Object element = sel.getFirstElement();
+				if (element instanceof ChangeMessageInfo) {
+					ChangeMessageInfo changeMessage = (ChangeMessageInfo) element;
+					msgTextData.setText(changeMessage.getMessage());
+					//set the size when data change
+					sc_msgtxt.setMinSize(msgTextData.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+				}
+			}
+		});
+
+		//Build a text area to display the whole message
+		sc_msgtxt = new ScrolledComposite(sashForm, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		sc_msgtxt.setExpandHorizontal(true);
+		sc_msgtxt.setExpandVertical(true);
+		sc_msgtxt.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 3, 1));
+
+		msgTextData = new Text(sc_msgtxt, SWT.BORDER | SWT.WRAP | SWT.MULTI);
+		msgTextData.setEditable(false);
+		sc_msgtxt.setContent(msgTextData);
+		sc_msgtxt.setMinSize(msgTextData.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		sc_msgtxt.addControlListener(new ControlListener() {
+
+			@Override
+			public void controlResized(ControlEvent e) {
+				// ignore
+				sc_msgtxt.setMinSize(msgTextData.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+			}
+
+			@Override
+			public void controlMoved(ControlEvent e) {
+				// ignore
+
+			}
+		});
 
 		//Set the binding for this section
 		hisTabDataBindings(listMessages);
