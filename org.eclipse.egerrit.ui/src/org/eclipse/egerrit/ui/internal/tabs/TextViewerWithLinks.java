7cdf7967e9aae1c8b113563c35a4a99641f213ae
Jacques Bouthillier <lmcbout@gmail.com>
2016-01-06 08:47:55 -0500

    -Method required for databinding
    Change-Id: Iffad3d292a9492519e72d4f9f512ab06eb053b8d
    Signed-off-by: Jacques Bouthillier <lmcbout@gmail.com>

--- org.eclipse.egerrit.ui/src/org/eclipse/egerrit/ui/internal/tabs/TextViewerWithLinks.java
+++ org.eclipse.egerrit.ui/src/org/eclipse/egerrit/ui/internal/tabs/TextViewerWithLinks.java
@@ -11,6 +11,9 @@
 
 package org.eclipse.egerrit.ui.internal.tabs;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 import org.eclipse.jface.text.hyperlink.HyperlinkManager;
 import org.eclipse.swt.widgets.Composite;
 
@@ -18,6 +21,9 @@ import org.eclipse.swt.widgets.Composite;
  * A text viewer that will always show links
  */
 public class TextViewerWithLinks extends org.eclipse.jface.text.source.SourceViewer {
+	// used to fire events of registered properties
+	private transient PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
 	public TextViewerWithLinks(Composite composite, int styles) {
 		super(composite, null, styles);
 	}
@@ -26,5 +32,29 @@ public class TextViewerWithLinks extends org.eclipse.jface.text.source.SourceVie
 		fHyperlinkManager.uninstall();
 		fHyperlinkManager = new AlwaysOnLinkManager(HyperlinkManager.FIRST);
 		fHyperlinkManager.install(this, fHyperlinkPresenter, fHyperlinkDetectors, fHyperlinkStateMask);
+	}
+
+	/**
+	 * Needed by BeanPropertyListenerSupport.processListener() to add a property change
+	 *
+	 * @param propertyName
+	 * @param listener
+	 */
+	public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+		propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+	}
+
+	/**
+	 * Needed by BeanPropertyListenerSupport.processListener() to remove the property listener
+	 *
+	 * @param propertyName
+	 * @param listener
+	 */
+	public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+		propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
+	}
+
+	protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+		propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
 	}
 }
