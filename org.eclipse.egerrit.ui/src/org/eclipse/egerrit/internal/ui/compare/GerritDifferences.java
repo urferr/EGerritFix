9fd66c20d45a5ffe44057d03f362af6458f10bad
Pascal Rapicault <pascal@rapicorp.com>
2016-11-21 15:33:52 -0500

    Tuleap #679 - Compare editor does not show difference across a rename
    The constants used to identify the changes were causing the compare
    framework to detect our change like a 3 way compare
    (ContentMergeViewer#internalRefresh) which is incorrect.
    
    Change-Id: If1d133663023a61fc6d95794d19b212d9047733f

--- org.eclipse.egerrit.ui/src/org/eclipse/egerrit/internal/ui/compare/GerritDifferences.java
+++ org.eclipse.egerrit.ui/src/org/eclipse/egerrit/internal/ui/compare/GerritDifferences.java
@@ -17,35 +17,35 @@ public class GerritDifferences extends Differencer {
 	/**
 	 * Difference constant (value 0) indicating no difference.
 	 */
-	public static final int NO_CHANGE = 0;
+	public static final int NO_CHANGE = Differencer.NO_CHANGE;
 
 	/**
 	 * Difference constant (value 1) indicating one side was added.
 	 */
-	public static final int ADDITION = 1;
+	public static final int ADDITION = 16 + Differencer.ADDITION;
 
 	/**
 	 * Difference constant (value 2) indicating one side was removed.
 	 */
-	public static final int DELETION = 2;
+	public static final int DELETION = 32 + Differencer.DELETION;
 
 	/**
 	 * Difference constant (value 3) indicating side changed.
 	 */
-	public static final int CHANGE = 3;
+	public static final int CHANGE = 64 + Differencer.CHANGE;
 
 	/**
 	 * Difference constant (value 4) indicating renaming.
 	 */
-	public static final int RENAMED = 4;
+	public static final int RENAMED = 128;
 
 	/**
 	 * Difference constant (value 5) indicating renaming.
 	 */
-	public static final int COPIED = 5;
+	public static final int COPIED = 256;
 
 	/**
 	 * Difference constant (value 6) indicating renaming.
 	 */
-	public static final int REWRITTEN = 6;
+	public static final int REWRITTEN = 512;
 }
